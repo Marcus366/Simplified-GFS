1f0b25424d58c36ec28af770219388e768eb5ab0
HandsomeCharming <jerry111zhang@163.com>
2014-06-02 23:22:45 +0800

    fixed open(halfway)

--- MasterServer/file.h
+++ MasterServer/file.h
@@ -4,6 +4,10 @@
 #include <fcntl.h>
 #include "gfs_filetree.h"
 
+
+extern struct gfs_list_s;
+typedef struct gfs_list_s gfs_list_t;
+
 #define FILE_TYPE_FILE 1
 #define FILE_TYPE_FOLDER 2
 #define MAX_FILE_SIZE 100000
@@ -12,7 +16,7 @@
 typedef struct file_s {
 	char name[33]; /* with at most 32 byte length name */
 	int type; /* file type */
-	/* list of chunks here */
+	gfs_list_t *chunks;/* list of chunks here */
 } file_t;
 
 
@@ -23,7 +27,7 @@ extern void file_new(file_t **file, char* name, int type);
 extern void file_free(file_t **file);
 
 
-extern void file_create(const char *path, mode_t mode, int type, gfs_node_t* root);   /* create a file and add it to the filetree */
+extern file_t *file_create(const char *path, mode_t mode, int type, gfs_node_t* root);   /* create a file and add it to the filetree */
 extern int file_equal(file_t* ,file_t* );  /* return 1 when equal, 0 when not equal. */
 extern int get_fd(file_t* file);		/* return fd, and also put the fd in the fds array. */
 extern int binary_search_fd(int fd);    /* binary search for fd in fds in case of conflict */
